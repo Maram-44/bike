03f608962309b6524405c353f318e22ee2b416f2
Manar <Manar.alnahdi25@gmail.com>
2025-04-26 22:14:37 +0300

    import page

---
+++ bike/importPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+
+namespace bike
+{
+
+    public partial class importPage : Window
+    {
+        public importPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ClickCount == 2)
+            {
+                this.WindowState=WindowState.Normal;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
